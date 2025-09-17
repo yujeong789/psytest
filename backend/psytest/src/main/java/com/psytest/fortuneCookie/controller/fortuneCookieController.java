9c61cf553425eb31bfc285f689ee42f75088276d
YuJeong <hao_yj@naver.com>
2025-09-17 23:45:58 +0900

    [be] 크로스오리진 설정

--- backend/psytest/src/main/java/com/psytest/fortuneCookie/controller/fortuneCookieController.java
+++ backend/psytest/src/main/java/com/psytest/fortuneCookie/controller/fortuneCookieController.java
@@ -5,12 +5,14 @@ import com.psytest.fortuneCookie.service.fortuneCookieService;
 import com.psytest.global.util.ResponseUtil;
 import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/fortuneCookie")
+@CrossOrigin(origins = "*")
 public class fortuneCookieController {
 
     private final fortuneCookieService fortuneCookieService;
