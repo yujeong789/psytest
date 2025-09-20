76e5a925718a4d86fede50aa13a620cd8288776e
YuJeong <hao_yj@naver.com>
2025-09-20 09:52:46 +0900

    [be] 포춭쿠키 문구 메모리에 저장(캐싱), 내 결과 공유를 위한 uuid 생성 -> 추후에 nanoId로 변경해서 더 길이를 줄일 예정

--- backend/psytest/src/main/java/com/psytest/fortuneCookie/service/fortuneCookieService.java
+++ backend/psytest/src/main/java/com/psytest/fortuneCookie/service/fortuneCookieService.java
@@ -1,8 +1,7 @@
 package com.psytest.fortuneCookie.service;
 
 import com.psytest.fortuneCookie.dto.FortuneCookieResponse;
-import com.psytest.fortuneCookie.entity.FortuneCookieEntity;
-import com.psytest.fortuneCookie.repository.fortuneCookieRepository;
+import com.psytest.fortuneCookie.dto.FortuneCookieCacheResponse;
 import com.psytest.global.exception.ErrorCode;
 import com.psytest.global.exception.GlobalException;
 import lombok.RequiredArgsConstructor;
@@ -11,29 +10,35 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import java.util.Random;
+import java.util.UUID;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class fortuneCookieService {
 
-    private final fortuneCookieRepository fortuneCookieRepository;
+    private final fortuneCookieCache fortuneCookieCache;
 
     public FortuneCookieResponse fortuneCookieOpen() {
-        log.info("😊 포춘쿠키를 깨트립니다.");
+        log.info("🥨포춘쿠키를 깨트립니다.");
 
-        log.info("😊 랜덤 인덱스를 선택합니다.");
+        log.info("🥨 랜덤 인덱스를 선택합니다.");
         Random ramdom = new Random();
         int idx = ramdom.nextInt(500)+1; // 1-500 사이의 랜덤 숫자
+        UUID fortuneCookieUuid = UUID.randomUUID();
 
-        log.info("😊 행운의 문구를 가져옵니다.");
-        FortuneCookieEntity fortuneCookieEntity = fortuneCookieRepository.findByFortuneCookieId(idx);
-        if (fortuneCookieEntity == null) {
+        log.info("🥨 행운의 문구를 가져옵니다.");
+        FortuneCookieCacheResponse fortuneCookieCacheResponse = fortuneCookieCache.getFortuneCookieOne(idx);
+        // FortuneCookieEntity fortuneCookieEntity = fortuneCookieRepository.findByFortuneCookieId(idx);
+        if (fortuneCookieCacheResponse == null) {
             log.warn("❌ fortuneCookieEntity 조회 실패. 행운의 문구를 가져오지 못했습니다. idx={}", idx);
             throw new GlobalException(HttpStatus.NOT_FOUND, ErrorCode.DATA_NOT_FOUND);
         }
 
-        log.info("😊 행운의 문구 : " + fortuneCookieEntity.getFortune());
-        return new FortuneCookieResponse(fortuneCookieEntity.getFortune());
+        log.info("🥨 행운의 문구 : " + fortuneCookieCacheResponse.fortune());
+        return new FortuneCookieResponse(
+                fortuneCookieUuid,
+                fortuneCookieCacheResponse.fortune()
+        );
     }
 }
