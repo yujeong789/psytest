9b0a8a9747d889184c1bdca2d88ef9f36bb98885
YuJeong <hao_yj@naver.com>
2025-09-24 21:49:06 +0900

    [be] CORS 오설정 수정

--- backend/psytest/src/main/java/com/psytest/global/config/WebConfig.java
+++ backend/psytest/src/main/java/com/psytest/global/config/WebConfig.java
@@ -10,7 +10,10 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // CORS를 사용할 URL패턴 정의
-                .allowedOrigins("*") // localhost:8080과 같이 자원 공유를 허락할 Origin 지정 가능
+                .allowedOrigins(
+                        "http://localhost:5173",
+                        "https://${HOST}:${PORT}"
+                ) // localhost:8080과 같이 자원 공유를 허락할 Origin 지정 가능
                 .allowedMethods("GET", "POST", "PUT", "DELETE") // 허용할 HTTP 메서드 지정
                 .allowedHeaders("Authorization", "Content-Type") // 헤더 지정
                 .exposedHeaders("Custom-Header") // 클라이언트 측 응답에서 노출되는 헤더 지정
