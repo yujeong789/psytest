9738a8967a43c31b909b7797bc9bf99a51cbd270
YuJeong <hao_yj@naver.com>
2025-09-28 21:19:12 +0900

    [fe] 시작 페이지 변경

--- frontend/psytest/src/pages/HomePage.tsx
+++ frontend/psytest/src/pages/HomePage.tsx
@@ -5,22 +5,26 @@ export default function HomePage() {
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-[#fff5e6] text-center px-4">
-      <div className="text-4xl font-bold mb-8">심리 테스트</div>
+      <div className="text-xmd font-bold">🥠🥠🥠🥠🥠🥠🥠</div>
+      <div className="text-xmd font-bold">🥠🥠🥠🥠🥠🥠🥠</div>
+      <div className="text-md font-bold ">🥠🥠포춘쿠키🥠🥠</div>
+      <div className="text-xmd font-bold">🥠🥠🥠🥠🥠🥠🥠</div>
+      <div className="text-md font-bold mb-4">🥠🥠🥠🥠🥠🥠🥠</div>
       <div className="justify-center">
         
         {/* 첫번째 버튼 */}
         <div className="flex  items-center justify-between">
-          <h1 className="m-3 text-xl sm:text-2xl md:text-3xl font-bold">행운의 쿠키</h1>
           <button
             onClick={() => navigate("/fortuneCookie")}
-            className="text-sm px-3 py-3 bg-orange-400 text-white rounded-lg shadow hover:bg-orange-5000"
+            className="text-xs p-1 bg-orange-400 rounded-lg shadow text-orange-500 bg-white hover:bg-orange-400 hover:text-white"
           >
-            쿠키 깨기
+            {/* group inline-flex items-center gap-2 px-3 py-3 rounded-3xl bg-white hover:bg-orange-400 text-white */}
+            🔨 깨러가기 🔨
           </button>
         </div>
 
         {/* 두번째 버튼 */}
-        <div className="flex  items-center justify-between">
+        {/* <div className="flex  items-center justify-between">
           <h1 className="m-3 text-xl sm:text-2xl md:text-3xl font-bold">오늘의 운세 (준비중...)</h1>
           <button
             onClick={() => navigate("/")}
@@ -28,7 +32,7 @@ export default function HomePage() {
           >
             운세 보기
           </button>
-        </div>
+        </div> */}
         
       </div>
     </div>
